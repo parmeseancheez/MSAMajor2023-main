3a0bad5ac13dfb060dff427f5539badd4fcf0cb2
parmeseancheez <68348181+parmeseancheez@users.noreply.github.com>
2023-06-17 10:21:54 -0500

    fg

--- vending_machine.py
+++ vending_machine.py
@@ -1,7 +1,15 @@
+def awaitCoin():
+    while True:
+        userInput = int(input("Please insert coins:\n"))
+        if userInput not in [1,5,10,25]:
+            print("Must input 1, 5, 10, or 25")
+        else:
+            return userInput
+     
+
 def awaitCost(cost):
     print(f"um gimme {cost} (in cents)")
     amountInserted = 0
     while amountInserted < cost:
-        userInput = input(f"Amount due: {cost-amountInserted}\nPlease insert coins:\n")
-        amountInserted+=userInput
+        amountInserted+=awaitCoin()
 awaitCost(50)
