aa0336fbb29123a14f931d2f453a095f985ecb5f
parmeseancheez <68348181+parmeseancheez@users.noreply.github.com>
2023-06-17 10:16:48 -0500

    bruh

--- vending_machine.py
+++ vending_machine.py
@@ -1,8 +1,7 @@
-cost = 50
-print(f"um gimme {cost} (in cents)")
-
-def awaitCost():
+def awaitCost(cost):
+    print(f"um gimme {cost} (in cents)")
     amountInserted = 0
     while amountInserted < cost:
         userInput = input(f"Amount due: {cost-amountInserted}\nPlease insert coins:\n")
         amountInserted+=userInput
+awaitCost(50)
