3cf649057312bb6b9c40c2763aaf4784930f7412
parmeseancheez <68348181+parmeseancheez@users.noreply.github.com>
2023-06-22 09:05:48 -0500

    bryyg

--- automobile.py
+++ automobile.py
@@ -1,10 +1,17 @@
 class Automobile():
     #Define a constructor
     #The constructor defines what happens when we create an automobile
-    def __init__(self, make, model, vin, engine_size, owner):
+    def __init__(self, make, model, vin, engine_size, owner, year):
         #assign parameter values
         self.make = make
         self.model = model
         self.vin = vin
         self.engine_size = engine_size
         self.owner = owner
+        self.year = year
+
+    #Create a method to print Automobile data
+    def printData(self):
+        print(f"{self.year} {self.make} {self.model}")
+        print(f"VIN: {self.vin}, Engine Size: {self.engine_size}")
+        print(f"Owner: {self.owner}\n")
--- automobile_generator.py
+++ automobile_generator.py
@@ -1,14 +1,13 @@
 import automobile
 
 #Create an automobile
-auto1 = automobile.Automobile("Mercury")
-auto2 = automobile.Automobile("Honda")
+auto1 = automobile.Automobile("Mercury", "Sable", "1234", 3.0, "Bob", 2000)
+auto2 = automobile.Automobile("Honda", "Accord", "23456", 1234, "Alice", 2003)
 
 auto_list = []
 auto_list.append(auto1)
 auto_list.append(auto2)
 
+#Print each automobile's info
 for auto in auto_list:
-    print(auto.make)
-    print(auto.model)
-    print()
+    auto.print_data()
