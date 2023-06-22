0412e80e966b0368da52c09eaf2c74ab2e415daf
parmeseancheez <68348181+parmeseancheez@users.noreply.github.com>
2023-06-22 09:46:42 -0500

    bruh

--- automobile.py
+++ automobile.py
@@ -1,14 +1,45 @@
 class Automobile():
+    #1. Polymorphism
+    #2. Inheritance
+    #3. Encapsulation
+
     #Define a constructor
     #The constructor defines what happens when we create an automobile
     def __init__(self, make, model, vin, engine_size, owner, year):
         #assign parameter values
-        self.make = make
-        self.model = model
-        self.vin = vin
-        self.engine_size = engine_size
-        self.owner = owner
-        self.year = year
+        self.__make = make
+        self.__model = model
+        self.__vin = vin
+        self.__engine_size = engine_size
+        self.__owner = owner
+        self.__year = year
+    
+    def get_make(self):
+        return self.__make
+    
+    def get_model(self):
+        return self.__model
+    
+    def get_vin(self):
+        return self.__vin
+    
+    def get_engine_size(self):
+        return self.__engine_size
+    
+    def set_engine_size(self, new_size):
+        self.__engine_size = new_size
+    
+    def get_owner(self):
+        return self.__owner
+
+    def set_owner(self, new_owner):
+        self.__owner = new_owner
+    
+    def get_year(self):
+        return self.__year
+    
+    def get_age(self, current_year):
+        return current_year - self.get_year()
 
     #Create a method to print Automobile data
     def printData(self):
--- automobile_generator.py
+++ automobile_generator.py
@@ -4,6 +4,9 @@ import automobile
 auto1 = automobile.Automobile("Mercury", "Sable", "1234", 3.0, "Bob", 2000)
 auto2 = automobile.Automobile("Honda", "Accord", "23456", 1234, "Alice", 2003)
 
+#Change auto2 year
+auto2.year = 2020
+
 auto_list = []
 auto_list.append(auto1)
 auto_list.append(auto2)
@@ -11,3 +14,5 @@ auto_list.append(auto2)
 #Print each automobile's info
 for auto in auto_list:
     auto.print_data()
+
+print(f"Auto1 is {auto1.get_age()} years old")
