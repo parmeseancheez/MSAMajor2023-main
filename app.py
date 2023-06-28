1b5aa789c3a6e44dafee0d2f49b3562dad865758
parmeseancheez <sccajigal@gmail.com>
2023-06-28 11:29:43 -0500

    gdfgdfg

--- app.py
+++ app.py
@@ -25,7 +25,43 @@ def index():
     #make a request to the student api url
     url = 'http://127.0.0.1:5000/api/students/all'
 
+    #get student data
     student_data = get_student_data(url)
+
     return render_template('index.html', student_data = student_data)
 
+@app.route('/majors', methods=['GET','POST'])
+def majors():
+    #make a request to the student api url
+    url = 'http://127.0.0.1:5000/api/students/all'
+
+    #get student data
+    student_data = get_student_data(url)
+
+    #student_data = [*range(0,10000000, 3)]
+    
+    #Write code that gets the unique majors from the student_data list
+    major_list = []
+    for student in student_data:
+        if student['major'] not in major_list:
+            major_list.append(student['major'])
+    
+    if request.method == 'POST':
+        #get the form data
+        major = request.form['major'].strip()
+
+        result_list = []
+        if not major:
+            #flash("You must a select a major")
+            pass
+        else:
+            #get student with selected major and palce in result list
+            for student in student_data:
+                if student['major'] == major:
+                    result_list.append(student)
+            return render_template('majors.html', major_list = major_list, result_list = result_list)
+
+    return render_template('majors.html',major_list=major_list)
+
+#Run the flask application
 app.run(port=5007)
